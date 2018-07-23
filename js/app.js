2a06a44b8b986a77a151dbf661ce77ece9204695
Pablito14 <pablo.rosales14@gmail.com>
2018-07-23 16:44:36 -0700

    Starting product objects

--- js/app.js
+++ js/app.js
@@ -1,4 +1,17 @@
-'use strict'
+'use strict';
+
+var listOfProducts =[];
+var totalProducts = 0;
+var totalVotes = 0;
+
+function Product(name, path){
+  this.name = name;
+  this.views = 0;
+  this.votes = 0;
+  this.path = path;
+  listOfProducts.push(this);
+  totalProducts++;
+}
 
 
 
