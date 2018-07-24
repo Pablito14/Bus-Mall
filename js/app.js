7ad952a3eec19e7c50bd7f037c219f98cc0eddf2
Pablito14 <pablo.rosales14@gmail.com>
2018-07-24 13:15:07 -0700

    Made some progress

--- js/app.js
+++ js/app.js
@@ -1,25 +1,39 @@
 'use strict';
 
-var listOfProducts =[];
-var totalProducts = 0;
-var totalVotes = 0;
+var productNames =[];
+
+var allProducts=[];
+var left = document.getElementById();
+var center = document.getElementById();
+var right = document.getElementById();
+var container = document.getElementById('container');
 
 function Product(name, path){
   this.name = name;
-  this.views = 0;
-  this.votes = 0;
   this.path = path;
+  this.views = 0;
+  this.clicks = 0;
   listOfProducts.push(this);
-  totalProducts++;
 }
 
-function uniqueRandomizer(){
-  for(var i = 0; i < listOfProducts.length; i++){
-    for(var j = i + 1; j < listOfProducts.length; j++){
-      if(listOfProducts[i] === listOfProducts[j]){
-        return true;
-      }
-    }
-    return false;
-  }
+for (var i = 0; i <productNames.length; i++){
+  new Product(productNames)[i];
 }
+
+function rando(){
+return Math.floor(Math.random() * allProducts.length);
+
+}
+
+
+function threeRandomImages(){
+left.src = allProducts[rando()].path;
+center.src = allProducts[rando()].path;
+right.src = allProducts[rando()].path;
+}
+
+function handleClick(event){
+  console.log(event.target.id, 'was clicked');
+  threeRandomImages();
+}
+
